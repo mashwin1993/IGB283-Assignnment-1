67099db119b7b90777c2c9515883b6e095a54bba
Jesse <jes.97@live.com.au>
2017-09-11 01:19:10 +1000

    Side count changing
    put in the code to change the number of sides the shape has. can be seen
    in the inspector by changing the value of sides changing the bool to
    true. just needs to be hooked to a slider (should only call when the
    slider has changed). a coded min of 3 (otherwise, what shape) and max of
    50 (performance and unneccesary detail. working alright up to 100, but
    already quite circular at 20)

--- Scripts/Triangle.cs
+++ Scripts/Triangle.cs
@@ -7,6 +7,8 @@ public class Triangle : MonoBehaviour
 {
     //Public Variables
 
+    public bool reshape;
+    public int sides = 3;
 
     //Locations
     public Vector2 pos1;
@@ -30,17 +32,16 @@ public class Triangle : MonoBehaviour
     public Material material;
     public Mesh mesh;
 
-    //Offset for Rotation
-    private Vector3 offset;
-
     //Public Rotation Angle Attach to slider
     public float RotateAngle;
 
     //Rotation Slider
     public Slider RotateSlider;
 
+    //Color Variables
+    new Color color1;
+    new Color color2;
 
-    
 
     public Vector3 centre
     {
@@ -62,12 +63,19 @@ public class Triangle : MonoBehaviour
         point1 = PointAt(pos1);
         point2 = PointAt(pos2);
 
+        SpeedSlider.value = 1.0f;
+
         SwitchTarget();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (reshape) {
+            reshape = false;
+            Reshape(sides);
+        }
+
         if (ReachedTarget())
         {
             SwitchTarget();
@@ -83,16 +91,29 @@ public class Triangle : MonoBehaviour
 
         meshTransform.Translate(direction.normalized * speed * Time.deltaTime);
 
+        //Maybe do something with scaling?
         meshTransform.Scale(scale);
 
         meshTransform.RotateCenter(RotateAngle);
+
+        //Color Changing with Lerp
+
+        //Probably should initialise as variables so getcomponent isn't called each update
+        color1 = point1.GetComponent<MeshRenderer>().material.color;
+        color2 = point2.GetComponent<MeshRenderer>().material.color;
+
+        float totalDist = Vector3.Distance(point1.GetComponent<Point>().centre, point2.GetComponent<Point>().centre);
+        float remDist = Vector3.Distance(centre, point2.GetComponent<Point>().centre);
+
+        float t = (totalDist - remDist) / totalDist;
+
+        gameObject.GetComponent<MeshRenderer>().material.color = Color.Lerp(color1, color2, t);
     }
 
 
     // Draw a rectangle
     public void Draw()
     {
-
         //Adding MeshFilter and MeshRenderer  to Empty GameObject
         gameObject.AddComponent<MeshFilter>();
         gameObject.AddComponent<MeshRenderer>();
@@ -103,34 +124,8 @@ public class Triangle : MonoBehaviour
         //Set the material to selected material 
         GetComponent<MeshRenderer>().material = material;
 
-        //Clear all data from the mesh
-        mesh.Clear();
-
-        //Set triangle points 
-        mesh.vertices = new Vector3[]
-        {
-            new Vector3(0, 0, size),
-            new Vector3(0, size, size),
-            new Vector3(size, size, size),
-            new Vector3(size, 0, size)
-        };
-
-        // Set colours of the triangle
-        mesh.colors = new Color[]
-        {
-            new Color(1.0f, 1.0f, 1.0f, 1.0f),
-            new Color(1.0f, 1.0f, 1.0f, 1.0f),
-            new Color(1.0f, 1.0f, 1.0f, 1.0f),
-            new Color(1.0f, 1.0f, 1.0f, 1.0f)
-        };
-
-
-        // Set vertex indicies
-        mesh.triangles = new int[] { 0, 1, 2, 0, 2, 3 };
-
-        // Calculate the bounds of the rectangle
-        offset.x = mesh.bounds.size.x / 2;
-        offset.y = mesh.bounds.size.y / 2;
+        //Make new mesh for object
+        Reshape(sides);
     }
 
     GameObject PointAt(Vector2 pos)
@@ -143,6 +138,7 @@ public class Triangle : MonoBehaviour
 
     void SwitchTarget()
     {
+        //Initialise/Switch between targets
         if (!currentTarget || currentTarget == point1.transform)
         {
             currentTarget = point2.transform;
@@ -152,11 +148,75 @@ public class Triangle : MonoBehaviour
             currentTarget = point1.transform;
         }
 
+        //Get target's script for easier reference
         targetPoint = currentTarget.GetComponent<Point>();
     }
 
     bool ReachedTarget()
     {
+        //Confirm whether the target is in touching distance of the frame
         return Vector3.Distance(centre, targetPoint.centre) < speed * Time.deltaTime;
+    }
+
+    void Reshape(int sides) {
+        //Reject values too low or high
+        if (sides < 3 || sides > 50) {
+            Debug.LogErrorFormat("{0} is an invalid number of sides for the shape to have.", sides);
+            return;
+        }
+
+        //Get the current position to move the new mesh to
+        Vector2 position = new Vector2(centre.x, centre.y);
+
+        List<Vector3> verts = new List<Vector3>();
+        //Add centre vertex
+        verts.Add(Vector3.zero);
+
+        //Add surrounding vertices
+        for (int i = 0; i < sides; i++) {
+            verts.Add(new Vector3(Mathf.Sin(i * 2 * Mathf.PI / sides), Mathf.Cos(i * 2 * Mathf.PI / sides), 1));
+        }
+    
+        List<int> tris = new List<int>();
+        //Add initial (outlier) tri
+        tris.Add(0);
+        tris.Add(1);
+        tris.Add(mesh.vertexCount - 1);
+
+        //Add fanning out tris
+        for (int i = 1; i < sides; i++) {
+            tris.Add(0);
+            tris.Add(i);
+            tris.Add(i + 1);
+        }
+
+        List<Color> colours = new List<Color>();
+        //Set all vertex colours to default
+        for (int i = 0; i < mesh.vertexCount; i++) {
+            colours.Add(Color.white);
+        }
+
+        //Send values to operation method
+        RecreateMesh(verts.ToArray(), tris.ToArray(), colours.ToArray());
+
+        //Move new mesh to previous position
+        meshTransform.Translate(position);
+    }
+
+    void RecreateMesh(Vector3[] verts, int[] tris, Color[] colours) {
+        //Clear all data from the mesh
+        mesh.Clear();
+
+        //Add vertices to mesh
+        mesh.vertices = verts;
+
+        //Add triangles to mesh
+        mesh.triangles = tris;
+
+        //Add colours to mesh
+        mesh.colors = colours;
+
+        //Recalculate the bounds of the mesh
+        mesh.RecalculateBounds();
     }
 }
